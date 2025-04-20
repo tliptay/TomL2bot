c594b2a20003e3a786dbb8dfe207a40a0afce461
tliptay <52239432+tliptay@users.noreply.github.com>
2025-04-20 09:56:58 -0400

    edit llms

--- main.py
+++ main.py
@@ -364,6 +364,10 @@ if __name__ == "__main__":
         #     ),
         #     "summarizer": "openai/gpt-4o-mini",
         # },
+        llms={ # LLM models to use for different tasks. Will use default llms if not specified. Requires the relevant provider environment variables to be set.
+            "default": GeneralLlm(model="anthropic/claude-3-5-sonnet-20240620", temperature=0),
+            "summarizer": "openai/gpt-4o-mini",
+        }
     )
 
     if run_mode == "tournament":
