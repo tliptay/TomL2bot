a651c612799bc6280ba8a684e34a67f876796351
tliptay <52239432+tliptay@users.noreply.github.com>
2025-04-20 21:50:52 -0400

    update to 5 predictions and skip previously forecast Qs

--- main.py
+++ main.py
@@ -424,11 +424,11 @@ if __name__ == "__main__":
 
     template_bot = TemplateForecaster(
         research_reports_per_question=1,
-        predictions_per_research_report=1, # NEED TO REVERT TO 3 
+        predictions_per_research_report=5,
         use_research_summary_to_forecast=False,
         publish_reports_to_metaculus=True,
         folder_to_save_reports_to=None,
-        skip_previously_forecasted_questions=False, # NEED TO REVERT
+        skip_previously_forecasted_questions=True,
         # llms={  # choose your model names or GeneralLlm llms here, otherwise defaults will be chosen for you
         #     "default": GeneralLlm(
         #         model="metaculus/anthropic/claude-3-5-sonnet-20241022",
